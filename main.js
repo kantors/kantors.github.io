66e0a6ca58ae50270c309b48531858f8dbfce537
Sabrina Kantor <kantor.sa@husky.neu.edu>
2017-10-04 01:05:55 -0400

    waiting for image to load

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
+// ==UserScript==
+// @name          Test
+// @namespace     Test
+// @include       *
+// ==/UserScript==
+
+window.addEventListener('load', function(){
+  setTimeout(function() {
+      $(".header .color-block").css("width", "0%");
+
+  }, 450);
+
+  setTimeout(function() {
+      $(".header .text-wrapper .text").css("width", "100%");
+
+  }, 1100);
+  setTimeout(function() {
+      $(".header .sub-text-wrapper .text").css("width", "100%");
+  }, 1400);
+  setTimeout(function() {
+      $(".header .arrow-wrapper ").addClass("visible-arrow");
+
+  }, 1900);
+  setTimeout(function() {
+      // $(".header .arrow-wrapper .about-me ").addClass("bounce");
+
+  }, 2400);
+
+
+}, false);
+
 $(document).ready(function() {
 
     /********HEADER ANIMATION********/
-    setTimeout(function() {
-        $(".header .color-block").css("width", "0%");
 
-    }, 450);
-
-    setTimeout(function() {
-        $(".header .text-wrapper .text").css("width", "100%");
-
-    }, 1100);
-    setTimeout(function() {
-        $(".header .sub-text-wrapper .text").css("width", "100%");
-    }, 1400);
-    setTimeout(function() {
-        $(".header .arrow-wrapper ").addClass("visible-arrow");
-
-    }, 1900);
-    setTimeout(function() {
-        // $(".header .arrow-wrapper .about-me ").addClass("bounce");
-
-    }, 2400);
 
     $("#work-link").click(function() {
         var offset = 20; //Offset of 20px
